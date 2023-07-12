900ca05777ff65ab386d9afa14fc7e354d17d686
unknown <i2225401@continental.edu.pe>
2023-07-11 19:18:08 -0500

    Update: Dashboard Admin  - Se creo un nuevo dashboard  - Se cambio las variables de colores

--- app/admin/page.jsx
+++ app/admin/page.jsx
@@ -1,92 +1,229 @@
 "use client";
 
 import ConductoresPage from "@/components/dashboard/ConductoresPage";
-import ConfiguracionPage from "@/components/dashboard/ConfiguracionPage";
-import FaqPage from "@/components/dashboard/FaqPage";
 import InicioPage from "@/components/dashboard/InicioPage";
-import PropietariosPage from "@/components/dashboard/PropietariosPage";
-import VehiculosPage from "@/components/dashboard/VehiculosPage";
 import { useState } from "react";
 
 export default function adminPage() {
-  const [activePage, setActivePage] = useState(1);
+  const [showAside, setShowAside] = useState(false);
+  const [darkMode, setDarkMode] = useState(false);
+  const [indexPage, setIndexPage] = useState(0);
+
+  const togglePages = (index) => {
+    setIndexPage(index);
+  };
+
+  const toggleTheme = () => {
+    setDarkMode(!darkMode);
+  };
+
+  const toggleMenu = () => {
+    setShowAside(!showAside);
+  };
 
   return (
-    <div className="container_admin">
-      <div className="sidebar_section">
-        <div className="top_section">
-          <div className="card_admin">
-            <div className="user_image">
-              <img src="/img/user.png" alt="user" />
+    <div className={darkMode ? "dashboard dark-theme-variables" : "dashboard"}>
+      <div className="container">
+        <aside className={showAside ? "active-aside" : ""}>
+          <div className="top">
+            <div className="logo">
+              <img src="/images/logo.png" />
+              <h2>
+                Capital<span className="danger">Tours</span>
+              </h2>
             </div>
-            <div className="user_name">
-              <h4>Manolo Rojas</h4>
-              <p>Supervisor</p>
+            <div className="close" id="close-btn" onClick={() => toggleMenu()}>
+              <i className="bx bx-x" />
             </div>
           </div>
-        </div>
-        <div className="body_section">
-          <div className="card_admin">
-            <ul>
-              <li
-                className={activePage === 1 ? "active" : ""}
-                onClick={() => setActivePage(1)}
-              >
-                <i className="bx bx-home" />
-                <a>Inicio</a>
-              </li>
-              <li
-                className={activePage === 2 ? "active" : ""}
-                onClick={() => setActivePage(2)}
-              >
-                <i className="bx bxs-car-garage" />
-                <a>Vehículos</a>
-              </li>
-              <li
-                className={activePage === 3 ? "active" : ""}
-                onClick={() => setActivePage(3)}
-              >
-                <i className="bx bxs-user-rectangle" />
-                <a>Propietarios</a>
-              </li>
-              <li
-                className={activePage === 4 ? "active" : ""}
-                onClick={() => setActivePage(4)}
-              >
-                <i className="bx bx-user" />
-                <a>Conductores</a>
-              </li>
-              <hr className="divider" />
-              <li
-                className={activePage === 5 ? "active" : ""}
-                onClick={() => setActivePage(5)}
-              >
-                <i className="bx bxs-cog" />
-                <a>Configuración</a>
-              </li>
-              <li
-                className={activePage === 6 ? "active" : ""}
-                onClick={() => setActivePage(6)}
-              >
-                <i className="bx bx-cog" />
-                <a>FAQ</a>
-              </li>
-            </ul>
+          <div className="sidebar">
+            <a
+              className={indexPage === 0 ? "active" : ""}
+              onClick={() => togglePages(0)}
+            >
+              <i className="bx bxs-dashboard" />
+              <h3>Dashboard</h3>
+            </a>
+            <a
+              className={indexPage === 1 ? "active" : ""}
+              onClick={() => togglePages(1)}
+            >
+              <i className="bx bx-user" />
+              <h3>Customers</h3>
+            </a>
+            <a
+              className={indexPage === 2 ? "active" : ""}
+              onClick={() => togglePages(2)}
+            >
+              <i className="bx bxs-receipt" />
+              <h3>Orders</h3>
+            </a>
+            <a
+              className={indexPage === 3 ? "active" : ""}
+              onClick={() => togglePages(3)}
+            >
+              <i className="bx bx-stats" />
+              <h3>Analytics</h3>
+            </a>
+            <a
+              className={indexPage === 4 ? "active" : ""}
+              onClick={() => togglePages(4)}
+            >
+              <i className="bx bx-message-dots" />
+              <h3>Messages</h3>
+              <span className="message-count">26</span>
+            </a>
+            <a
+              className={indexPage === 5 ? "active" : ""}
+              onClick={() => togglePages(5)}
+            >
+              <i className="bx bx-purchase-tag-alt" />
+              <h3>Producs</h3>
+            </a>
+            <a
+              className={indexPage === 6 ? "active" : ""}
+              onClick={() => togglePages(6)}
+            >
+              <i className="bx bx-spreadsheet" />
+              <h3>Reports</h3>
+            </a>
+            <a
+              className={indexPage === 7 ? "active" : ""}
+              onClick={() => togglePages(7)}
+            >
+              <i className="bx bx-cog" />
+              <h3>Settings</h3>
+            </a>
+            <a
+              className={indexPage === 8 ? "active" : ""}
+              onClick={() => togglePages(8)}
+            >
+              <i className="bx bx-plus" />
+              <h3>Add Product</h3>
+            </a>
+            <a>
+              <i className="bx bx-log-out" />
+              <h3>Logout</h3>
+            </a>
           </div>
+        </aside>
+        {/* ------------------------- END OF ASIDE ------------------------- */}
+        <div>
+          {indexPage === 0 && <InicioPage />}
+          {indexPage === 1 && <ConductoresPage />}
         </div>
-      </div>
-      <div className="pages_section">
-        <div className="top_pages">
-          <div className="card_admin"></div>
-        </div>
-        <div className="body_pages">
-          <div className="card_admin">
-            {activePage === 1 && <InicioPage />}
-            {activePage === 2 && <VehiculosPage />}
-            {activePage === 3 && <PropietariosPage />}
-            {activePage === 4 && <ConductoresPage />}
-            {activePage === 5 && <ConfiguracionPage />}
-            {activePage === 6 && <FaqPage />}
+        {/* ------------ END OF MAIN ------------ */}
+        <div className="right">
+          <div className="top">
+            <button id="menu-btn" onClick={() => toggleMenu()}>
+              <i className="bx bx-menu" />
+            </button>
+            <div className="theme-toggler" onClick={() => toggleTheme()}>
+              <i className={!darkMode ? "bx bxs-sun active" : "bx bxs-sun"} />
+              <i className={darkMode ? "bx bxs-moon active" : "bx bxs-moon"} />
+            </div>
+            <div className="profile">
+              <div className="info">
+                <p>
+                  Hey, <b>Josh</b>
+                </p>
+                <small className="text-muted">Supervisor</small>
+              </div>
+              <div className="profile-photo">
+                <img src="/images/profile-1.jpg" />
+              </div>
+            </div>
+          </div>
+          {/* ------------ END OF TOP ------------ */}
+          <div className="recent-updates">
+            <h2>Recent Updates</h2>
+            <div className="updates">
+              <div className="update">
+                <div className="profile-photo">
+                  <img src="/images/profile-2.jpg" />
+                </div>
+                <div className="message">
+                  <p>
+                    <b>Mike Tyson</b> received his order of Night lion tech GPS
+                    drone
+                  </p>
+                  <small className="text-muted">2 Minutes Ago</small>
+                </div>
+              </div>
+              <div className="update">
+                <div className="profile-photo">
+                  <img src="/images/profile-3.jpg" />
+                </div>
+                <div className="message">
+                  <p>
+                    <b>Mike Tyson</b> received his order of Night lion tech GPS
+                    drone
+                  </p>
+                  <small className="text-muted">2 Minutes Ago</small>
+                </div>
+              </div>
+              <div className="update">
+                <div className="profile-photo">
+                  <img src="/images/profile-4.jpg" />
+                </div>
+                <div className="message">
+                  <p>
+                    <b>Mike Tyson</b> received his order of Night lion tech GPS
+                    drone
+                  </p>
+                  <small className="text-muted">2 Minutes Ago</small>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* ------------ END OF RECENT UPDATES ------------ */}
+          <div className="sales-analytics">
+            <h2>Sales Analytics</h2>
+            <div className="item online">
+              <div className="icon">
+                <i className="bx bxs-cart" />
+              </div>
+              <div className="right">
+                <div className="info">
+                  <h3>ONLINE ORDERS</h3>
+                  <small className="text-muted">Last 24 Hours</small>
+                </div>
+                <h5 className="success">+39%</h5>
+                <h3>3849</h3>
+              </div>
+            </div>
+            <div className="item offline">
+              <div className="icon">
+                <i className="bx bxs-shopping-bag" />
+              </div>
+              <div className="right">
+                <div className="info">
+                  <h3>OFFLINE ORDERS</h3>
+                  <small className="text-muted">Last 24 Hours</small>
+                </div>
+                <h5 className="danger">-17%</h5>
+                <h3>1100</h3>
+              </div>
+            </div>
+            <div className="item customers">
+              <div className="icon">
+                <i className="bx bxs-user" />
+              </div>
+              <div className="right">
+                <div className="info">
+                  <h3>NEW CUSTOMERS</h3>
+                  <small className="text-muted">Last 24 Hours</small>
+                </div>
+                <h5 className="success">+25%</h5>
+                <h3>849</h3>
+              </div>
+            </div>
+            <div className="item add-product">
+              <div>
+                <i className="bx bx-plus" />
+                <h3>Add Product</h3>
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- components/dashboard/InicioPage.jsx
+++ components/dashboard/InicioPage.jsx
@@ -1,7 +1,122 @@
 export default function InicioPage() {
   return (
-    <div className="section_inicio">
-      INICIO
+    <div className="main">
+      <h1>Dashboard</h1>
+      <div className="date">
+        <input type="date" />
+      </div>
+      <div className="insights">
+        <div className="sales">
+          <i className="bx bx-bar-chart-square" />
+          <div className="middle">
+            <div className="left">
+              <h3>Total Sales</h3>
+              <h1>S/.25.024</h1>
+            </div>
+            <div className="progress">
+              <svg>
+                <circle cx="38" cy="38" r="36"></circle>
+              </svg>
+              <div className="number">
+                <p>81%</p>
+              </div>
+            </div>
+          </div>
+          <small className="text-muted">Last 24 Hours</small>
+        </div>
+        {/* ------------ END OF SALES ------------ */}
+        <div className="expenses">
+          <i className="bx bx-bar-chart-square" />
+          <div className="middle">
+            <div className="left">
+              <h3>Total Expenses</h3>
+              <h1>S/.14.160</h1>
+            </div>
+            <div className="progress">
+              <svg>
+                <circle cx="38" cy="38" r="36"></circle>
+              </svg>
+              <div className="number">
+                <p>62%</p>
+              </div>
+            </div>
+          </div>
+          <small className="text-muted">Last 24 Hours</small>
+        </div>
+        {/* ------------ END OF EXPENSES ------------ */}
+        <div className="income">
+          <i className="bx bx-bar-chart-square" />
+          <div className="middle">
+            <div className="left">
+              <h3>Total Income</h3>
+              <h1>S/.10.864</h1>
+            </div>
+            <div className="progress">
+              <svg>
+                <circle cx="38" cy="38" r="36"></circle>
+              </svg>
+              <div className="number">
+                <p>44%</p>
+              </div>
+            </div>
+          </div>
+          <small className="text-muted">Last 24 Hours</small>
+        </div>
+        {/* ------------ END OF INCOME ------------ */}
+      </div>
+      {/* ------------ END OF INSIGHTS ------------ */}
+      <div className="recent-orders">
+        <h2>Recent Orders</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>Product Name</th>
+              <th>Product Number</th>
+              <th>Payment</th>
+              <th>Status</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Foldable Mini Drone</td>
+              <td>86531</td>
+              <td>Due</td>
+              <td className="warning">Pending</td>
+              <td className="primary">Details</td>
+            </tr>
+            <tr>
+              <td>Foldable Mini Drone</td>
+              <td>86531</td>
+              <td>Due</td>
+              <td className="warning">Pending</td>
+              <td className="primary">Details</td>
+            </tr>
+            <tr>
+              <td>Foldable Mini Drone</td>
+              <td>86531</td>
+              <td>Due</td>
+              <td className="warning">Pending</td>
+              <td className="primary">Details</td>
+            </tr>
+            <tr>
+              <td>Foldable Mini Drone</td>
+              <td>86531</td>
+              <td>Due</td>
+              <td className="warning">Pending</td>
+              <td className="primary">Details</td>
+            </tr>
+            <tr>
+              <td>Foldable Mini Drone</td>
+              <td>86531</td>
+              <td>Due</td>
+              <td className="warning">Pending</td>
+              <td className="primary">Details</td>
+            </tr>
+          </tbody>
+        </table>
+        <a href="#">Show All</a>
+      </div>
     </div>
-  )
+  );
 }
